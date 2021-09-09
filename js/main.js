10d688363a574355ef36ade1888235712ff55bd2
Marlon Molina <molinamarlon@gmail.com>
2021-09-09 07:17:22 -0600

    Ofertar
    html - Css - Js

--- js/main.js
+++ js/main.js
@@ -24,3 +24,20 @@ if (option_value == "addNew") {
    $("#exampleModal").modal();
  }
 }
+// Get the modal
+var modal = document.getElementById("myModal");
+
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+var span = document.getElementsByClassName("closeX")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
